195b6e598e99bf6bb80648f7b9398733e3486875
buchuitoudegou <756541536@qq.com>
2019-05-05 16:19:17 +0800

    add dev dependencies

---
+++ vue.config.js
@@ -0,0 +1,63 @@
+const merge = require('webpack-merge');
+const fs = require('fs');
+const path = require('path');
+const lessToJs = require('less-vars-to-js');
+// const apiMocker = require('webpack-api-mocker');
+
+// const lessGlobalVarsString = fs.readFileSync(
+//   'src/global-variables.less',
+//   'utf8'
+// );
+// fs.writeFileSync(
+//   'src/global-variables.ts',
+//   `/* tslint:disable */\nexport default ${JSON.stringify(
+//     lessToJs(lessGlobalVarsString, {
+//       resolveVariables: true,
+//       stripPrefix: true
+//     })
+//   )};`,
+//   'utf8'
+// );
+
+module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule('ts')
+      .use('ts-loader')
+      .tap(options =>
+        merge(options, {
+          getCustomTransformers: () => ({
+            before: [
+              require('ts-import-plugin')([
+                // {
+                //   libraryName: 'element-ui',
+                //   styleLibraryName: 'theme-chalk'
+                // },
+                {
+                  style: false,
+                  libraryName: 'lodash',
+                  libraryDirectory: null,
+                  camel2DashComponentName: false
+                }
+              ])
+            ]
+          })
+        })
+      );
+  },
+  // css: {
+  //   loaderOptions: {
+  //     less: {
+  //       modifyVars: lessToJs(lessGlobalVarsString)
+  //     }
+  //   }
+  // },
+  devServer: {
+    proxy: 'http://193.112.59.226'
+    // before(app) {
+    //   if (process.env.MOCK_DATA) {
+    //     apiMocker(app, path.resolve(__dirname, './mock/index.js'));
+    //   }
+    // }
+  }
+};
