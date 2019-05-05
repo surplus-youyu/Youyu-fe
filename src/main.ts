49e9f22abe9c3e0109ef287631b41650e2630902
buchuitoudegou <756541536@qq.com>
2019-05-05 16:21:02 +0800

    fix tslint

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import Vue from 'vue';
 import App from './App.vue';
+import Vue from 'vue';
 import router from './views/router';
 import store from './stores';
 
@@ -8,5 +8,5 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: (h) => h(App)
 }).$mount('#app');
