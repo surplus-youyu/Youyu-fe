0dc030e219778b84cc57f812a2b118a0dbffddf8
unknown <756541536@qq.com>
2019-05-09 22:17:22 +0800

    update login

---
+++ src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'login'
+})
+export default class Login extends Vue {
+  
+}
+</script>
