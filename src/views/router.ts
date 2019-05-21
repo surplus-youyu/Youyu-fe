b0873db4df99eaf5b46c6d463ee448c0c945f2b9
7cthunder <32539531@qq.com>
2019-05-21 17:34:28 +0800

    modify router.js

--- src/views/router.ts
+++ src/views/router.ts
@@ -1,7 +1,6 @@
 import Home from './Home.vue';
 import Router from 'vue-router';
 import Vue from 'vue';
-import BasicLayout from '@/layouts/BasicLayout.vue';
 
 Vue.use(Router);
 
@@ -18,27 +17,6 @@ export default new Router({
       path: '/signup',
       name: 'signup',
       component: () => import('./Signup.vue')
-    },
-    {
-      path: '/',
-      component: BasicLayout,
-      children: [
-        {
-          path: '',
-          name: 'home',
-          redirect: '/requestHall'
-        },
-        {
-          path: 'requestHall',
-          name: 'requestHall',
-          component: () => import('./requestHall/requestHall.vue')
-        },
-        {
-          path: 'about',
-          name: 'about',
-          component: () => import('./about/about.vue')
-        }
-      ]
     }
   ]
 });
