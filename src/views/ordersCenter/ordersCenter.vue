91ae659dcb9062332f7cfc026c6c9384172a1be4
ninanshoulewozaizhe <172685516@qq.com>
2019-05-27 22:04:54 +0800

    调整菜单

---
+++ src/views/ordersCenter/ordersCenter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>orders center</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'ordersCenter'
+})
+export default class OrdersCenter extends Vue {
+
+}
+</script>
+
---
+++ src/views/publish/publish.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>publish</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'publish'
+})
+export default class Publish extends Vue {
+
+}
+</script>
+
