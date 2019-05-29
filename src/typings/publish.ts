2dd270ba86d5ecf7648413c65a54f94f0b957bdf
buchuitoudegou <756541536@qq.com>
2019-05-29 17:48:46 +0800

    feat: typing of questionare

---
+++ src/typings/publish.ts
@@ -0,0 +1,14 @@
+export interface IQuestionare {
+  id: number;
+  title: string;
+  publisher_id: number;
+  content: IQuestionareContent[];
+}
+
+export interface IQuestionareContent {
+  title: string;
+  type: 1 | 2 | 3; // 单选 1, 多选 2, 填空 3
+  options: string[];
+  optional: boolean;
+  limit: number;
+}
