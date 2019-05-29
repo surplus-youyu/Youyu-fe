20a1076296d1b1b876340d504e92f67a1bacda71
buchuitoudegou <756541536@qq.com>
2019-05-29 17:49:19 +0800

    feat: update publish

--- src/views/publish/publish.vue
+++ src/views/publish/publish.vue
@@ -1,15 +1,19 @@
 <template>
-    <div>publish</div>
+  <question-creater/>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { IQuestionareContent } from '@/typings/publish';
+import QuestionCreater from '@/components/QuestionCreater.vue';
 
 @Component({
-  name: 'publish'
+  name: 'publish',
+  components: {
+    QuestionCreater
+  }
 })
 export default class Publish extends Vue {
-
 }
 </script>
 
