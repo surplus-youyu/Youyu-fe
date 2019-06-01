957fcf1af60c3b82b1f5acc574e16746d38c159d
ninanshoulewozaizhe <172685516@qq.com>
2019-06-01 11:11:12 +0800

    布局调整

---
+++ src/shims-module.d.ts
@@ -0,0 +1,19 @@
+declare module '*.svg' {
+    const url: string;
+    export default url;
+}
+
+declare module '*.jpg' {
+const url: string;
+export default url;
+}
+
+declare module '*.png' {
+const url: string;
+export default url;
+}
+
+declare module '*.json' {
+const content: object;
+export default content;
+}
--- src/stores/modules/user/typing.ts
+++ src/stores/modules/user/typing.ts
@@ -6,6 +6,7 @@ export interface IUserInfo {
   uid: number;
   real_name: string;
   nick_name: string;
+  avatar?: string;
   age: number;
   gender: 'm' | 'f';
   balance: number;
