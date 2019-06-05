f2923f71b2adc85ad04d88604d157ff2234e5327
7cthunder <32539531@qq.com>
2019-06-05 17:02:38 +0800

    add bounty input

--- src/views/publish/PublishCustomTask.vue
+++ src/views/publish/PublishCustomTask.vue
@@ -1,22 +1,30 @@
 <template>
   <div class="publish-customtask">
-    <h1 style="text-align: center;">创建你的任务</h1>
+    <h1 style="margin-top: 1rem; text-align: center;">创建你的任务</h1>
     <div class="title-wrapper">
       <h3 style="margin-bottom: 1rem">标题</h3>
       <Input 
-        v-model="taskTitle" type="text" 
+        v-model="currentCustomTask.title" type="text" 
         placeholder="请输入任务标题"
-        style="width: 60%"
+        style="width: 55%"
         size="large"/>
     </div>
     <div class="description-wrapper">
       <h3 style="margin-bottom: 1rem">描述</h3>
       <Input 
-        v-model="taskDescription" 
+        v-model="currentCustomTask.description" 
         type="textarea" 
         :rows="6" 
         placeholder="请输入任务描述..." 
-        style="width: 60%"/>
+        style="width: 55%"/>
+    </div>
+    <div class="payment-wrapper">
+      <h3 style="margin-bottom: 1rem">报酬</h3>
+      <Input 
+        v-model="currentCustomTask.bounty" 
+        prefix="logo-yen"
+        style="width: 55%"
+        size="large"/>
     </div>
     <div class="uploader-wrapper">
       <h3 style="margin-bottom: 1rem">附件</h3>
@@ -25,7 +33,7 @@
         multiple
         action=""
         type="drag"
-        style="width: 60%">
+        style="width: 55%">
         <div style="padding: 20px 0">
           <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
           <p>点击或拖拽上传附件</p>
@@ -52,7 +60,15 @@ import { POST_QUESTIONARE } from '@/stores/modules/questionnaire/constants';
 export default class Publish extends Vue {
   taskTitle: string = '';
   taskDescription: string = '';
+  taskPayment: number = 0;
   file: any = null;
+
+  currentCustomTask: IQuestionnaire = {
+    title: '',
+    publisher_id: -1,
+    summary: '',
+    bounty: 0
+  };
 
   handleUpload(file: any) {
     this.file = file;
@@ -62,6 +78,7 @@ export default class Publish extends Vue {
   clear() {
     this.taskTitle = '';
     this.taskDescription = '';
+    this.taskPayment = 0;
     this.file = null;
   }
 
@@ -100,24 +117,30 @@ export default class Publish extends Vue {
 .submit-btn-wrapper,
 .title-wrapper,
 .description-wrapper,
+.payment-wrapper,
 .uploader-wrapper
 {
   margin: 1rem 0 0 0;
 }
 
-.title-wrapper, .description-wrapper, .uploader-wrapper {
+.title-wrapper, 
+.description-wrapper,
+.payment-wrapper,
+.uploader-wrapper 
+{
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
   h3 {
     text-align: left;
-    width: 60%;
+    width: 55%;
   }
 }
 
 .submit-btn-wrapper {
   display: flex;
   justify-content: flex-end;
+  margin-top: 3rem;
 }
 </style>
--- src/views/publish/PublishQuestionnaire.vue
+++ src/views/publish/PublishQuestionnaire.vue
@@ -9,21 +9,29 @@
     <div style="position: fixed; bottom: 10vh; z-index: 100;" >
       <Button type="primary" icon="md-add" shape="circle" @click="createQuestion" size="large"></Button>
     </div>
-    <h1 style="margin-bottom: 1rem;">创建你的问卷</h1>
+    <h1 style="margin-top: 1rem; margin-bottom: 1rem; text-align: center;">创建你的问卷</h1>
     <div class="title-wrapper">
       <h3 style="margin-bottom: 1rem">标题</h3>
       <Input 
-        type="text" 
-        placeholder="输入问卷标题" 
-        style="width: 60%;"
+        type="text"
+        placeholder="请输入问卷标题..." 
+        style="width: 55%;"
         v-model="currentQuestionnaire.title"
         size="large"/>
       <h3 style="margin: 1rem 0 1rem 0">描述</h3>
       <Input
-        type="text"
-        placeholder="输入问卷描述"
-        style="width: 60%"
-        v-model="currentQuestionnaire.summary"
+        type="textarea"
+        :rows="6"
+        placeholder="请输入问卷描述"
+        style="width: 55%"
+        v-model="currentQuestionnaire.summary"/>
+    </div>
+    <div class="bounty-wrapper">
+      <h3 style="margin-bottom: 1rem">报酬</h3>
+      <Input 
+        v-model="currentQuestionnaire.bounty" 
+        prefix="logo-yen"
+        style="width: 55%"
         size="large"/>
     </div>
     <div class="questions-wrapper">
@@ -33,7 +41,7 @@
         你的问卷还没有任何问题哦，点击左下方的“+”按钮创建吧！
       </h3>
       <h3 v-else>问题</h3>
-      <Card :dis-hover="true" style="margin: 1rem 0 0 0;" 
+      <Card :dis-hover="true" style="margin: 1rem 0 0 0; width: 55%" 
         v-for="(content, idx) in currentQuestionnaire.content" :key="'content' + idx">
         <question-form 
         :inputContent="content"
@@ -84,11 +92,13 @@ export default class Publish extends Vue {
 
   newQuestionHandler(content: IQuestionnaireContent) {
     this.showCreaterDialog = false;
-    this.currentQuestionnaire.content.push(content);
+    if (this.currentQuestionnaire.content)
+      this.currentQuestionnaire.content.push(content);
   }
 
   modifyQuestionHandler(content: IQuestionnaireContent, index: number) {
-    this.currentQuestionnaire.content[index] = content;
+    if (this.currentQuestionnaire.content)
+      this.currentQuestionnaire.content[index] = content;
   }
 
   createQuestion() {
@@ -108,7 +118,7 @@ export default class Publish extends Vue {
         duration: 2
       });
       return;
-    } else if (this.currentQuestionnaire.content.length === 0) {
+    } else if (this.currentQuestionnaire.content && this.currentQuestionnaire.content.length === 0) {
       this.$Notice.error({
         title: '题目不能为空',
         duration: 2
@@ -151,9 +161,26 @@ export default class Publish extends Vue {
 </script>
 <style lang="less">
 .submit-btn-wrapper,
-.questions-wrapper 
+.questions-wrapper,
+.bounty-wrapper
 {
   margin: 1rem 0 0 0;
+}
+
+.title-wrapper, 
+.description-wrapper,
+.bounty-wrapper, 
+.uploader-wrapper, 
+.questions-wrapper 
+{
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  h3 {
+    text-align: left;
+    width: 55%;
+  }
 }
 
 .submit-btn-wrapper {
