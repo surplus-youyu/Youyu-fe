067349524670d1c241aeaed89bbff4b0355dfbff
buchuitoudegou <756541536@qq.com>
2019-06-14 23:48:25 +0800

    feat: overview and detail of questionnaire

--- src/views/answer/Survey.vue
+++ src/views/answer/Survey.vue
@@ -1,15 +1,31 @@
 <template>
   <div id="current-questionnaire">
+    <h1>{{currentQuestionnaire.title}}</h1>
+    <h3>{{currentQuestionnaire.summary}}</h3>
     <answer-sheet v-for="(content, idx) in currentQuestionnaire.content" ref="answer"
-    :key="`content-questionnaire-${idx}`" :inputQuestion="content" :index="idx"></answer-sheet>
-    <Button @click="submit">提交</Button>
+    :key="`content-questionnaire-${idx}`" :inputQuestion="content" :index="idx"
+    :readonly="readonly"></answer-sheet>
+    <Button v-if="!readonly" @click="submit">提交</Button>
+    <Button v-else @click="acceptAndAnswer">接受并任务并开始填写</Button>
   </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { IQuestionnaire } from '../../typings/publish';
 import AnswerSheet from '@/components/AnswerSheet.vue';
-import { LOAD_QUESTIONARE, GET_CURRENT_QUESTIONARE } from '@/stores/modules/questionnaire/constants';
+import {
+  LOAD_QUESTIONARE,
+  GET_CURRENT_QUESTIONARE,
+  RECEIVE_QUESTIONARE
+} from '@/stores/modules/questionnaire/constants';
+import {
+  LOAD_ALL_ASSIGNMENTS,
+  GET_ALL_ASSIGNMENTS,
+  SUBMIT_ASSIGNMENT_ANSWER
+} from '../../stores/modules/assignment/constants';
+import { IAssignment } from '../../typings/assignment';
+import { ITask } from '../../typings/task';
+import assignment from '../../stores/modules/assignment';
 
 @Component({
   name: 'survey',
@@ -17,13 +33,16 @@ import { LOAD_QUESTIONARE, GET_CURRENT_QUESTIONARE } from '@/stores/modules/ques
     AnswerSheet
   },
   async beforeRouteUpdate(to: any, from: any, next: any) {
-    if (to.params.sid !== from.params.sid) {
+    if (this.$route.name === 'view-questionnaire' && to.params.sid !== from.params.sid) {
       await this.$store.dispatch(`questionnaire/${LOAD_QUESTIONARE}`, to.params.sid);
     }
     next();
   }
 })
 export default class Survey extends Vue {
+  get readonly() {
+    return this.$route.name === 'view-questionnaire';
+  }
   currentQuestionnaire: IQuestionnaire = {
     title: '',
     publisher_id: -1,
@@ -32,7 +51,7 @@ export default class Survey extends Vue {
     content: []
   };
 
-  submit() {
+  async submit() {
     const arr: AnswerSheet[] = (this.$refs.answer as AnswerSheet[]);
     let isValid: boolean = true;
     arr.forEach((component, idx) => {
@@ -45,12 +64,55 @@ export default class Survey extends Vue {
         title: '不满足问卷填写要求，无法提交',
         duration: 2
       });
+    } else {
+      const result = await this.$store.dispatch(`assignment/${SUBMIT_ASSIGNMENT_ANSWER}`, {
+        aid: this.$route.params.sid,
+        payload: this.currentQuestionnaire.content
+      });
+      if (result === 'OK') {
+        this.$Notice.success({
+          title: 'success'
+        });
+      } else {
+        this.$Notice.error({
+          title: 'fail'
+        });
+      }
     }
   }
 
   async mounted() {
-    await this.$store.dispatch(`questionnaire/${LOAD_QUESTIONARE}`, this.$route.params.sid);
-    this.currentQuestionnaire = this.$store.getters[`questionnaire/${GET_CURRENT_QUESTIONARE}`];
+    if (this.$route.name === 'view-questionnaire') {
+      await this.$store.dispatch(`questionnaire/${LOAD_QUESTIONARE}`, this.$route.params.sid);
+      this.currentQuestionnaire = this.$store.getters[`questionnaire/${GET_CURRENT_QUESTIONARE}`];
+    } else {
+      await this.$store.dispatch(`assignment/${LOAD_ALL_ASSIGNMENTS}`);
+      const assignments: IAssignment[] = this.$store.getters[`assignment/${GET_ALL_ASSIGNMENTS}`];
+      let sid = -1;
+      assignments.forEach((assign) => {
+        if (String(assign.id) === this.$route.params.aid) {
+          sid = assign.task_id;
+        }
+      });
+      await this.$store.dispatch(`questionnaire/${LOAD_QUESTIONARE}`, sid);
+      this.currentQuestionnaire = this.$store.getters[`questionnaire/${GET_CURRENT_QUESTIONARE}`];
+    }
+  }
+
+  async acceptAndAnswer() {
+    const result = await this.$store.dispatch(`questionnaire/${RECEIVE_QUESTIONARE}`, {
+      task_id: this.$route.params.sid
+    });
+    if (!isNaN(Number(result))) {
+      this.$router.push({
+        name: 'answer-questionnaire',
+        params: { aid: result }
+      });
+    } else {
+      this.$Notice.error({
+        title: 'fail'
+      });
+    }
   }
 }
 </script>
--- src/views/requestHall/requestHall.vue
+++ src/views/requestHall/requestHall.vue
@@ -1,20 +1,8 @@
 <template>
   <div>
-    <div class="sort-group">
-      <sort-button class="sort-btn" :reset="!sortBytime" title="时间" @status-change="selectTime"></sort-button>
-      <sort-button class="sort-btn" :reset="sortBytime" title="价格" @status-change="selectBounty"></sort-button>
-      <div class="select-container">
-        <Select class="select" clearable @on-change="selectCampus" style="width:200px" placeholder="校区">
-          <Option value="east" key="east">东校园</Option>
-          <Option value="north" key="north">北校园</Option>
-          <Option value="south" key="south">南校园</Option>
-          <Option value="Shenzhen" key="Shenzhen">深圳校园</Option>
-        </Select>
-      </div>
-      <Input class="search" type='text' @on-enter="search" v-model="searchText" clearable placeholder="请输入搜索内容" suffix="ios-search"/>
-    </div>
     <div class="cards-container">
-      <request-card class="req-card" v-for="(item, index) in ShowReqs" :key="index" :req="item"></request-card>
+      <request-card class="req-card" v-for="(task, index) in allTasks" 
+      :key="index" :req="task" @click="getTaskDetail(index)"></request-card>
     </div>
   </div>
 </template>
@@ -24,190 +12,62 @@ import { Component, Vue } from 'vue-property-decorator';
 import SortButton from '@/components/SortButton.vue';
 import RequestCard from '@/components/RequestCard.vue';
 import { SortMap, RequsetMsg } from '@/typings/requestHall';
-import {
-  LOAD_REQUESTS,
-  GET_PUBLIC_REQUESTS,
-  GET_SELF_REQUESTS_SENT,
-  GET_SELF_REQUESTS_RECEIVED,
-  GET_SELF_REQUESTS_DONE,
-  GET_SELF_REQUESTS_DRAFT
-  } from '@/stores/modules/requests/constants';
 import store from '@/stores';
 import { UID } from '@/stores/modules/user/constants';
-import { dataMap } from './constants';
+import { GET_ALL_TASKS, LOAD_ALL_TASKS } from '../../stores/modules/task/constants';
+import { ITask } from '@/typings/task';
+import { LOAD_ALL_ASSIGNMENTS, GET_ALL_ASSIGNMENTS } from '../../stores/modules/assignment/constants';
+import { IAssignment } from '../../typings/assignment';
 
 @Component({
   name: 'requestHall',
   components: {
     SortButton,
     RequestCard
-  },
-  async beforeRouteEnter(to: any, from: any, next: any) {
-    let data: RequestInfo[] = [];
-    if (to.params.requestType === 'public') {
-      const result = await store.dispatch(`requests/${LOAD_REQUESTS}`, 'public');
-      if (result) {
-        data = store.getters[`requests/${dataMap[to.params.requestType]}`];
-      }
-      next((vm: any) => {
-        vm.Reqs = [ ...data ];
-        vm.ShowReqs = [ ...data ];
-      });
-    } else {
-      const uid = store.getters[`user/${UID}`];
-      if (uid) {
-        const result = await store.dispatch(`requests/${LOAD_REQUESTS}`, to.params.requestType, uid);
-        if (result) {
-          data = store.getters[`requests/${dataMap[to.params.requestType]}`];
-          next((vm: any) => {
-          vm.Reqs = [ ...data ];
-          vm.ShowReqs = [ ...data ];
-        });
-        }
-      } else {
-        next('/login');
-      }
-    }
-  },
-  async beforeRouteUpdate(to: any, from: any, next: any) {
-    const data: RequestInfo[] = [];
-    if (to.params.requestType === 'public') {
-      const result = await store.dispatch(`requests/${LOAD_REQUESTS}`, 'public');
-      // if (result) {
-      //   data = store.getters[`requests/${dataMap[to.params.requestType]}`];
-      //   this.Reqs = [ ...data ];
-      //   this.ShowReqs = [ ...data ];
-      // }
-    } else {
-      const uid = store.getters[`user/${UID}`];
-      if (uid) {
-        const result = await store.dispatch(`requests/${LOAD_REQUESTS}`, to.params.requestType, uid);
-        // if (result) {
-        //   data = store.getters[`requests/${dataMap[to.params.requestType]}`];
-        //   this.Reqs = [ ...data ];
-        //   this.ShowReqs = [ ...data ];
-        // }
-      }
-    }
-    next();
   }
 })
 export default class RequestHall extends Vue {
-
-  sortBytime = false;
-  searchText = '';
-
-  sortMap: SortMap = {
-    time: 0,
-    price: 0,
-    campus: ''
-  };
-
-  Reqs: RequsetMsg[] = [];
-
-  ShowReqs: RequsetMsg[] = [];
-
-  selectTime(timeStatus: SortMap['time']) {
-    this.sortMap.time = timeStatus;
-    this.sortBytime = true;
-    this.sortList(false);
+  get allTasks(): ITask[] {
+    return this.$store.getters[`task/${GET_ALL_TASKS}`];
   }
 
-  selectBounty(bountyStatus: SortMap['price']) {
-    this.sortMap.price = bountyStatus;
-    this.sortBytime = false;
-    this.sortList(false);
-  }
-
-  selectCampus(campus: SortMap['campus']) {
-    // 清除campus选项时设为''
-    campus = campus || '';
-    this.sortMap.campus = campus;
-    this.sortList(true);
-  }
-
-  // 首先先根据校区进行筛选,再根据时间或价格进行排序
-  sortList(filterCampus: boolean) {
-    if (filterCampus) {
-      if (this.sortMap.campus) {
-        this.ShowReqs = this.Reqs.filter((item) => item.campus === this.sortMap.campus);
+  async getTaskDetail(index: number) {
+    if (this.allTasks[index].type === 'TASK_TYPE_SURVEY') {
+      await this.$store.dispatch(`assignment/${LOAD_ALL_ASSIGNMENTS}`);
+      const assignments: IAssignment[] = this.$store.getters[`assignment/${GET_ALL_ASSIGNMENTS}`];
+      let isReceived = false;
+      let aid = -1;
+      assignments.forEach((assignment) => {
+        if (assignment.task_id === this.allTasks[index].id) {
+          isReceived = true;
+          aid = assignment.id;
+        }
+      });
+      if (isReceived) {
+        this.$router.push({
+          name: 'answer-questionnaire',
+          params: {
+            aid: String(aid)
+          }
+        });
       } else {
-        this.ShowReqs = this.Reqs;
+        this.$router.push({
+          name: 'view-questionnaire',
+          params: {
+            sid: String(this.allTasks[index].id)
+          }
+        });
       }
     }
-    let sortType = '';
-    let sortOrder = 0;
-    if (this.sortMap.time && this.sortBytime) {
-      sortType = 'created_at';
-      sortOrder = this.sortMap.time;
-    } else if (this.sortMap.price && !this.sortBytime) {
-      sortType = 'bounty';
-      sortOrder = this.sortMap.price;
-    }
-    if (sortType && sortOrder) {
-      this.ShowReqs.sort((a: RequsetMsg, b: RequsetMsg) => {
-          if (a[sortType] > b[sortType]) {
-            return sortOrder === 1 ? -1 : 1;
-          } else if (a[sortType] < b[sortType]) {
-            return sortOrder === 1 ? 1 : -1;
-          } else {
-            return 0;
-          }
-      });
-    }
   }
 
-  search() {
-    // 避免上次搜索结果为空,而这次搜索拿空数据进行过滤
-    this.sortList(true);
-    this.ShowReqs = this.ShowReqs.filter((item) => item.title.includes(this.searchText));
-  }
-
-  mounted() {
-    const data = store.getters[`requests/${dataMap[this.$route.params.requestType]}`];
-    this.Reqs = [ ...data ];
-    this.ShowReqs = [ ...data ];
+  async mounted() {
+    await this.$store.dispatch(`task/${LOAD_ALL_TASKS}`);
   }
 }
 </script>
 
 <style lang="less" scoped>
-  .sort-group {
-    padding-left: 20px;
 
-    .sort-campus {
-      margin-left: 20px;
-
-      .icon {
-        position: relative;
-        top: -1px;
-        left: 2px;
-      }
-    }
-
-    .select-container {
-      display: inline-block;
-
-      .select {
-        margin-left: 10px;
-        width: 100px !important;
-      }
-    }
-
-    .search {
-      margin-left: 40px;
-      width: auto;
-    }
-    
-  }
-
-  .cards-container {
-    margin-top: 20px;
-    padding: 0 35px;
-
-    .req-card {
-      margin: 0 20px 20px 0;
-    }
-  }
 </style>
 
